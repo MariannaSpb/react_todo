b52cd28127b811c9d3f27d849687a807da5ce69f
Marianna Minich <marry-minich@ld.yandex.ru>
2020-02-06 16:35:55 +0300

    Updates

---
+++ precache-manifest.e4661f87a4fc749b4156c2bc29a7a6a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0647a471482d52b95e64551c6d6ca2e8",
+    "url": "/react_todo.github.io/index.html"
+  },
+  {
+    "revision": "a17946319fa651295f6b",
+    "url": "/react_todo.github.io/static/css/main.efe99581.chunk.css"
+  },
+  {
+    "revision": "9f09342b68b010cd1e88",
+    "url": "/react_todo.github.io/static/js/2.f0abb0c5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_todo.github.io/static/js/2.f0abb0c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a17946319fa651295f6b",
+    "url": "/react_todo.github.io/static/js/main.07340310.chunk.js"
+  },
+  {
+    "revision": "6522aa82a8d6a7d4b563",
+    "url": "/react_todo.github.io/static/js/runtime-main.105069ae.js"
+  }
+]);
